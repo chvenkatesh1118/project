e22b6c64f412b329947b0a9926aaa43f4d745248
Chanti <chvenkatesh.1118@gmail.com>
2022-07-22 19:46:03 +0530

    24-03

--- files/cluster.tf
+++ files/cluster.tf
@@ -37,13 +37,45 @@ resource "aws_ecs_capacity_provider" "EC2" {
 }
 
 
+
+
+
+
+
+resource "aws_launch_template" "launchtemplate" {
+  name_prefix   = "launchtemplate"
+  image_id      = "ami-0bb6af715826253bf"
+  instance_type = "t2.micro"
+}
+
 resource "aws_autoscaling_group" "autoscale" {
-  max_size = 1
-  min_size = 1
-  #mixed_instances_policy = false
-  tag {
-    key                 = "AmazonECSManaged"
-    value               = true
-    propagate_at_launch = true
+  capacity_rebalance  = true
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+  vpc_zone_identifier = [aws_subnet.subnet1.id]
+
+  mixed_instances_policy {
+    instances_distribution {
+      on_demand_base_capacity                  = 0
+      on_demand_percentage_above_base_capacity = 1
+      spot_allocation_strategy                 = "capacity-optimized"
+    }
+
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.launchtemplate.id
+      }
+
+      override {
+        instance_type     = "t2.micro"
+        weighted_capacity = "1"
+      }
+
+      override {
+        instance_type     = "t2.micro"
+        weighted_capacity = "1"
+      }
+    }
   }
 }
