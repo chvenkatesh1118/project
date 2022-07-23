d539c6d1b1fc02b125fcb9e577f18c7848fd204e
Chanti <chvenkatesh.1118@gmail.com>
2022-07-23 10:27:31 +0530

    24-03

--- files/role.tf
+++ files/role.tf
@@ -17,54 +17,95 @@ resource "aws_iam_role" "chantiecsrole" {
 }
 EOF
 }
-resource "aws_iam_role_policy_attachment"  "test-attach" {
-  role       = aws_iam_role.chantiecsrole.name
-  policy_arn = aws_iam_policy.ecspolicy.arn
-
-}
 
 resource "aws_iam_policy" "ecspolicy" {
   name        = "ecs_policy"
   path        = "/"
   description = "My test policy"
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
         "Sid" : "VisualEditor0",
         "Effect" : "Allow",
-        "Action" : "ecs:*"
-      },
-      {
+        "Action" : "ecs:*",
 
+        "Resource" : "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "cloudpolicy" {
+  name        = "cloudpolicy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
         "Effect" : "Allow",
         "Action" : "cloudformation:*",
         "Resource" : "*"
-      },
+      }
+    ]
+  })
+}
+resource "aws_iam_policy" "ec2policy" {
+  name        = "ec2policy"
+  path        = "/"
+  description = "My test policy"
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
         "Action" : "ec2:*",
         "Effect" : "Allow",
         "Resource" : "*"
       },
-
       {
         "Effect" : "Allow",
         "Action" : "elasticloadbalancing:*",
         "Resource" : "*"
       },
-
       {
         "Effect" : "Allow",
         "Action" : "cloudwatch:*",
         "Resource" : "*"
       },
-
       {
         "Effect" : "Allow",
         "Action" : "autoscaling:*",
         "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "iam:CreateServiceLinkedRole",
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "iam:AWSServiceName" : [
+              "autoscaling.amazonaws.com",
+              "ec2scheduled.amazonaws.com",
+              "elasticloadbalancing.amazonaws.com",
+              "spot.amazonaws.com",
+              "spotfleet.amazonaws.com",
+              "transitgateway.amazonaws.com"
+            ]
+          }
+        }
       },
     ]
   })
@@ -74,5 +115,8 @@ resource "aws_iam_policy" "ecspolicy" {
 
 
 
+resource "aws_iam_role_policy_attachment" "test-attach" {
+      role       = aws_iam_role.chantiecsrole.name
+      policy_arn = [aws_iam_policy.ecspolicy.arn, aws_iam_policy.ec2policy.arn, aws_iam_policy.cloudpolicy.arn]
 
-
+}
