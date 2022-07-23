dd4290dd2a3cd446c8019ad330e60928420fa032
Chanti <chvenkatesh.1118@gmail.com>
2022-07-23 09:57:31 +0530

    24-03

--- files/role.tf
+++ files/role.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "chantiecsrole" {
 EOF
 }
 
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "ecspolicy" {
   name        = "ecs_policy"
   path        = "/"
   description = "My test policy"
@@ -32,13 +32,84 @@ resource "aws_iam_policy" "policy" {
         "Sid" : "VisualEditor0",
         "Effect" : "Allow",
         "Action" : "ecs:*",
+
         "Resource" : "*"
+      },
+      {
+
+        "Effect": "Allow",
+        "Action": "cloudformation:*",
+        "Resource": "*"
       },
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.chantiecsrole.name
-  policy_arn = aws_iam_policy.policy.arn
-}
+
+resource "aws_iam_policy" "ec2policy" {
+  name        = "ec2policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "ec2:*",
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "elasticloadbalancing:*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "cloudwatch:*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "autoscaling:*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "iam:CreateServiceLinkedRole",
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "iam:AWSServiceName" : [
+              "autoscaling.amazonaws.com",
+              "ec2scheduled.amazonaws.com",
+              "elasticloadbalancing.amazonaws.com",
+              "spot.amazonaws.com",
+              "spotfleet.amazonaws.com",
+              "transitgateway.amazonaws.com"
+            ]
+          }
+        }
+      }
+    ]
+  }
+
+
+
+
+
+    resource "aws_iam_role_policy_attachment" "test-attach" {
+
+    }
+      role       = aws_iam_role.chantiecsrole.name
+      policy_arn = aws_iam_policy.ecspolicy.arn
+      policy_arn = aws_iam_policy.ec2policy.arn
+
+
+    }
+
+
+
--- files/service.tf
+++ files/service.tf
@@ -1,11 +1,13 @@
 
  resource "aws_ecs_service" "ecsservice" {
-   name            = "ecsservice"
-   cluster         = aws_ecs_cluster.ecscluster.id
+
+   launch_type     = "EC2"
    task_definition = aws_ecs_task_definition.taskdef.arn
-   desired_count   = 3
+   cluster         = aws_ecs_cluster.ecscluster.id
+   name            = "ecsservice"
+   desired_count   = 1
    iam_role        = aws_iam_role.chantiecsrole.arn
    #depends_on      = [aws_iam_role_policy.foo]
-   launch_type     = "EC2"
+
 
  }
