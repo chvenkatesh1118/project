a66c323d2344086e3ada3b1ecca5d7202d3de300
Chanti <chvenkatesh.1118@gmail.com>
2022-07-24 15:20:27 +0530

    24-03

--- files/service.tf
+++ files/service.tf
@@ -7,7 +7,7 @@
    name            = "ecsservice"
    desired_count   = 1
    iam_role        = aws_iam_role.chantiecsrole.arn
-   #depends_on      = [aws_iam_role_policy.foo]
+#   depends_on      = [aws_iam_role_policy.foo]
 
 
  }
