d75f969d4a68d98efaebced4726953ba705835b3
Chanti <chvenkatesh.1118@gmail.com>
2022-07-28 21:24:25 +0530

    24-03

--- service.tf
+++ service.tf
@@ -8,7 +8,13 @@
    desired_count   = 1
    iam_role        = aws_iam_role.chantiecsrole.arn
 #   depends_on      = [aws_iam_role_policy.ec2policy.arn]
-   load_balancer =  aws_lb.lb.id
+#   load_balancer =  aws_lb.lb.id
+
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.foo.arn
+#     container_name   = "nginx"
+#     container_port   = 8080
+#   }
 
 
  }
